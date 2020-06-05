73ced7526c19f9686af4d5fd461e13d90dd6b447
Jorge Navarro <jnavarr56@gmail.com>
2020-06-04 21:39:36 -0400

    rabbitmq vars

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import app from './app';
+import * as ampq from 'amqplib';
 import mongoose, { ConnectionOptions } from 'mongoose';
-import { PORT } from './vars';
+import { PORT, AUTHORIZATION_QUEUE_NAME, RABBIT_MQ_URL } from './vars';
 import { RedisClient } from 'redis';
 import RedisSingleton from './util/RedisSingleton';
 import * as dotenv from 'dotenv';
+import AuthTokenCache from './util/AuthTokenCache';
 
 dotenv.config();
 
@@ -11,7 +13,26 @@ const { MONGO_DB_URL } = process.env;
 
 const redisClient: RedisClient = new RedisSingleton().getInstance();
 
-redisClient.on('connect', () => {
+redisClient.on('connect', async () => {
+	const rabbitMQConnection: ampq.Connection = await ampq.connect(RABBIT_MQ_URL);
+	const rabbitMQChannel: ampq.Channel = await rabbitMQConnection.createChannel();
+	const authTokenCache: AuthTokenCache = new AuthTokenCache();
+
+	rabbitMQChannel.assertQueue(AUTHORIZATION_QUEUE_NAME, { durable: false });
+	rabbitMQChannel.prefetch(1);
+	rabbitMQChannel.consume(
+		AUTHORIZATION_QUEUE_NAME,
+		async (msg: ampq.ConsumeMessage | null) => {
+			if (msg) {
+				const token: string = await authTokenCache.generateSystemAuthToken();
+				rabbitMQChannel.sendToQueue(msg.properties.replyTo, new Buffer(token), {
+					correlationId: msg.properties.correlationId
+				});
+				rabbitMQChannel.ack(msg);
+			}
+		}
+	);
+
 	const dbURL = `${MONGO_DB_URL}/authentication-api?retryWrites=true&w=majority`;
 	const dbOptions: ConnectionOptions = {
 		useNewUrlParser: true,
@@ -19,17 +40,10 @@ redisClient.on('connect', () => {
 		useUnifiedTopology: true
 	};
 
-	mongoose
-		.connect(dbURL, dbOptions)
-		.then(() => {
-			app.listen(PORT, () => {
-				console.log(
-					`Authentication API running on ${PORT} of http://authentication-api!`
-				);
-			});
-		})
-		.catch((error: Error) => {
-			console.trace(error);
-			process.exit(1);
-		});
+	await mongoose.connect(dbURL, dbOptions);
+	app.listen(PORT, () => {
+		console.log(
+			`Authentication API running on ${PORT} of http://authentication-api!`
+		);
+	});
 });
--- src/vars.ts
+++ src/vars.ts
@@ -1,6 +1,8 @@
 export const PORT = 80;
 export const REDIS_PORT = 6379;
-export const REDIS_URL = 'redis://cache';
+export const REDIS_URL = 'redis://redis-auth';
+export const RABBIT_MQ_URL = 'amqp://rabbitmq-auth';
+export const AUTHORIZATION_QUEUE_NAME = 'AUTHORIZATION_QUEUE';
 export const PATHNAME = '/api/authentication';
 export const USERS_API = 'http://users-api/api/users';
 export const ACCESS_TOKEN_LIFE_SECS = 3600;
